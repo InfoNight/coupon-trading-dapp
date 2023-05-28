c2964a4bcf3c5274f5b6dfd432640c637e7f1f74
Kalogon <ghlee5084@kaist.ac.kr>
2023-05-28 19:31:40 +0900

    Bugfix: resolve getPinJsonByURI CORS error

--- src/utils/pinata.js
+++ src/utils/pinata.js
@@ -4,29 +4,18 @@ const secret = process.env.REACT_APP_PINATA_SECRET;
 const axios = require('axios');
 
 const getPinJsonByURI = async (couponURI) => {
-    const url = `https://api.pinata.cloud/data/pinList?status=pinned&hashContains=${couponURI.substring(34)}`;
-
     return axios
-        .get(url, {
+        .get(couponURI, {
             headers: {
-                pinata_api_key: key,
-                pinata_secret_api_key: secret,
-            }
+                'Content-Type': "application/json"
+            },
+            credentials: 'same-origin'
         })
         .then(function (response) {
-            if (response.data.count === 0) {
-                return {
-                    success: false,
-                    message: "No such coupon",
-                }
-            } else {
-                const pinJson = response.data.rows[0]
-                console.log(pinJson)
-                return {
-                    success: true,
-                    pinJson: pinJson,
-                };
-            }
+            return {
+                success: true,
+                pinJson: response.data,
+            };
         })
         .catch(function (error) {
             console.log(error)
@@ -36,29 +25,5 @@ const getPinJsonByURI = async (couponURI) => {
             }
         });
 };
-
-
-// const getPinJsonByURI = async (couponURI) => {
-//     return axios
-//         .get(couponURI, {
-//             headers: {
-//                 'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
-//                 "Access-Control-Allow-Origin": "https://gateway.pinata.cloud/ipfs/",
-//             }
-//         })
-//         .then(function (response) {
-//             return {
-//                 success: true,
-//                 pinJson: response.data,
-//             };
-//         })
-//         .catch(function (error) {
-//             console.log(error)
-//             return {
-//                 success: false,
-//                 message: error.message,
-//             }
-//         });
-// };
 
 export { getPinJsonByURI };
